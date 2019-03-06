32245b7ab8d8eaa620d7344fe7e0407b0dea2f05
francesco <frbracch@gmail.com>
2019-03-06 22:55:22 +0100

    meh

--- src/wabi_mem.h
+++ src/wabi_mem.h
@@ -7,8 +7,11 @@
 
 #include "wabi_types.h"
 
+#define  _LARGEFILE64_SOURCE
+#define  _FILE_OFFSET_BITS 64
+
 #define WABI_MEM_FROM 0x0010000000000000
-#define WABI_MEM_SIZE 0x00000000FFFFFFFF
+#define WABI_MEM_SIZE 0x00003FFFFFFFFFFF
 #define WABI_MEM_TO   (WABI_MEM_FROM + WABI_MEM_SIZE)
 #define WABI_MEM_HALF (WABI_MEM_SIZE / 2)
 
@@ -18,4 +21,5 @@ void wabi_mem_init(wabi_size_t size);
 wabi_word_t *wabi_mem_allocate(wabi_size_t size);
 wabi_word_t wabi_mem_used();
 void wabi_mem_collect();
+
 #endif
