f08d5464d6727f8819f1d1edd35803c18b9eea84
francesco <frbracch@gmail.com>
2019-08-18 23:17:56 +0200

    tests

--- src/wabi_io.c
+++ src/wabi_io.c
@@ -1,11 +1,19 @@
 #define wabi_io_c
 
+#include <stdio.h>
+
 #include "ev.h"
 #include "wabi_system.h"
 
-
 int wabi_loop(wabi_system sys)
 {
-  ev_loop loop;
+  struct ev_loop *loop;
+  wabi_system_t wabi_sys;
+
+  wabi_sys.config.store_initial_size = 10240;
+  wabi_system_init(&wabi_sys);
+
   printf("haha");
+
+  wabi_system_destroy(&wabi_sys);
 }
