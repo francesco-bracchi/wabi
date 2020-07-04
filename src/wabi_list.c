5cc53a52ff60bda4b4f444dd1d109fefea478278
francesco <frbracch@gmail.com>
2020-07-04 14:27:40 +0200

    list

---
+++ src/wabi_list.c
@@ -0,0 +1,157 @@
+#define wabi_list_c
+
+#include "wabi_list.h"
+#include "wabi_number.h"
+#include "wabi_builtin.h"
+#include "wabi_constant.h"
+#include "wabi_env.h"
+#include "wabi_error.h"
+#include "wabi_value.h"
+#include "wabi_vm.h"
+
+
+static void
+wabi_list_car(const wabi_vm vm) {
+  wabi_val ctrl, pair;
+  ctrl = vm->ctrl;
+
+  if (!wabi_is_pair(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  pair = wabi_car((wabi_pair)ctrl);
+  ctrl = wabi_cdr((wabi_pair)ctrl);
+  if (!wabi_is_empty(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  if (wabi_is_pair(pair)) {
+    vm->ctrl = (wabi_val)wabi_car((wabi_pair)pair);
+    vm->cont = (wabi_val)wabi_cont_next((wabi_cont)vm->cont);
+    return;
+  }
+  if (wabi_is_nil(pair)) {
+    vm->ctrl = vm->nil;
+    vm->cont = (wabi_val)wabi_cont_next((wabi_cont)vm->cont);
+    return;
+  }
+  vm->ert = wabi_error_type_mismatch;
+}
+
+
+static void
+wabi_list_cdr(const wabi_vm vm) {
+  wabi_val ctrl, pair;
+  ctrl = vm->ctrl;
+
+  if (!wabi_is_pair(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  pair = wabi_car((wabi_pair)ctrl);
+  ctrl = wabi_cdr((wabi_pair)ctrl);
+  if (!wabi_is_empty(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  if (wabi_is_pair(pair)) {
+    vm->ctrl = (wabi_val)wabi_cdr((wabi_pair)pair);
+    vm->cont = (wabi_val)wabi_cont_next((wabi_cont)vm->cont);
+    return;
+  }
+  if (wabi_is_nil(pair)) {
+    vm->ctrl = vm->nil;
+    vm->cont = (wabi_val)wabi_cont_next((wabi_cont)vm->cont);
+    return;
+  }
+  vm->ert = wabi_error_type_mismatch;
+}
+
+
+static void
+wabi_list_len(const wabi_vm vm) {
+  wabi_val ctrl, list;
+  wabi_size l;
+  wabi_fixnum r;
+
+  ctrl = vm->ctrl;
+
+  if (!wabi_is_pair(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  list = wabi_car((wabi_pair)ctrl);
+  ctrl = wabi_cdr((wabi_pair)ctrl);
+  if (!wabi_is_empty(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  l = wabi_list_length(list);
+  if(l < 0) {
+    vm->ert = wabi_error_type_mismatch;
+    return;
+  }
+  r = wabi_fixnum_new(vm, l);
+  if(vm->ert) return;
+  vm->ctrl = (wabi_val) r;
+  vm->cont = (wabi_val)wabi_cont_next((wabi_cont)vm->cont);
+}
+
+
+static void wabi_list_cons(const wabi_vm vm) {
+  wabi_val ctrl, a, d, res;
+
+  ctrl = vm->ctrl;
+  if (!wabi_is_pair(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  a = wabi_car((wabi_pair)ctrl);
+  ctrl = wabi_cdr((wabi_pair)ctrl);
+  if (!wabi_is_pair(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  d = wabi_car((wabi_pair)ctrl);
+  ctrl = wabi_cdr((wabi_pair)ctrl);
+  if (!wabi_is_empty(ctrl)) {
+    vm->ert = wabi_error_bindings;
+    return;
+  }
+  res = (wabi_val)wabi_cons(vm, a, d);
+  if (vm->ert)
+    return;
+  vm->ctrl = res;
+  vm->cont = (wabi_val)wabi_cont_next((wabi_cont)vm->cont);
+}
+
+
+static void wabi_list_pair_p(const wabi_vm vm)
+{
+  wabi_builtin_predicate(vm, &wabi_is_pair);
+}
+
+
+static void wabi_list_list_p(const wabi_vm vm)
+{
+  wabi_builtin_predicate(vm, &wabi_is_list);
+}
+
+
+void
+wabi_list_builtins(const wabi_vm vm,
+                   const wabi_env env)
+{
+  wabi_defn(vm,env, "cons", &wabi_list_cons);
+  if(vm->ert) return;
+  wabi_defn(vm,env, "car", &wabi_list_car);
+  if(vm->ert) return;
+  wabi_defn(vm,env, "cdr", &wabi_list_cdr);
+  if(vm->ert) return;
+  wabi_defn(vm,env, "pair?", &wabi_list_pair_p);
+  if(vm->ert) return;
+  wabi_defn(vm,env, "list?", &wabi_list_list_p);
+  if(vm->ert) return;
+  wabi_defn(vm,env, "len", &wabi_list_len);
+  if(vm->ert) return;
+}
---
+++ src/wabi_list.h
@@ -0,0 +1,129 @@
+#ifndef wabi_list_h
+
+#define wabi_list_h
+
+#include "wabi_value.h"
+#include "wabi_constant.h"
+#include "wabi_vm.h"
+#include "wabi_collect.h"
+#include "wabi_env.h"
+#include "wabi_cmp.h"
+
+typedef struct wabi_pair_struct
+{
+  wabi_word cdr;
+  wabi_word car;
+} wabi_pair_t;
+
+typedef wabi_pair_t* wabi_pair;
+
+#define WABI_PAIR_SIZE wabi_sizeof(wabi_pair_t)
+
+
+static inline wabi_pair
+wabi_cons(const wabi_vm vm,
+          const wabi_val car,
+          const wabi_val cdr)
+{
+  wabi_pair pair = (wabi_pair) wabi_vm_alloc(vm, WABI_PAIR_SIZE);
+  if(vm->ert) return NULL;
+
+  pair->car = (wabi_word) car;
+  pair->cdr = (wabi_word) cdr;
+  WABI_SET_TAG(pair, wabi_tag_pair);
+  return pair;
+}
+
+
+static inline wabi_val
+wabi_car(const wabi_pair pair)
+{
+  return (wabi_val) pair->car;
+}
+
+
+static inline wabi_val
+wabi_cdr(const wabi_pair pair)
+{
+  return (wabi_val) WABI_WORD_VAL(pair->cdr);
+}
+
+static inline void
+wabi_pair_copy_val(const wabi_vm vm,
+                   const wabi_pair pair)
+{
+  wabi_copy_val_size(vm, (wabi_val) pair, WABI_PAIR_SIZE);
+}
+
+static inline void
+wabi_pair_collect_val(const wabi_vm vm,
+                      const wabi_pair pair)
+{
+  wabi_collect_val_size(vm, (wabi_val) pair, WABI_PAIR_SIZE);
+}
+
+static inline void
+wabi_pair_hash(const wabi_hash_state state,
+               const wabi_pair pair) {
+  wabi_hash_step(state, "P", 1);
+  wabi_hash_val(state, wabi_car(pair));
+  wabi_hash_val(state, wabi_cdr(pair));
+}
+
+
+static inline int
+wabi_pair_cmp(const wabi_pair left, const wabi_pair right)
+{
+  int cmp0 = wabi_cmp(wabi_car(left), wabi_car(right));
+  if(cmp0) return cmp0;
+  return wabi_cmp(wabi_cdr(left), wabi_cdr(right));
+}
+
+void
+wabi_list_builtins(const wabi_vm vm, const wabi_env env);
+
+
+static inline int
+wabi_is_pair(const wabi_val v)
+{
+  return WABI_IS(wabi_tag_pair, v);
+}
+
+
+static inline wabi_size
+wabi_list_length(const wabi_val v0)
+{
+  wabi_val v;
+  wabi_size l;
+  v = v0;
+  l = 0;
+  for (;;) {
+    if(wabi_is_empty(v)) {
+      return l;
+    }
+    if(!wabi_is_pair(v)) {
+      return -1;
+    }
+    v = wabi_cdr((wabi_pair) v);
+    l++;
+  }
+}
+
+static inline int
+wabi_is_list(const wabi_val v0)
+{
+  wabi_val v;
+
+  v = v0;
+  for (;;) {
+    if(wabi_is_empty(v)) {
+      return 1;
+    }
+    if(!wabi_is_pair(v)) {
+      return 0;
+    }
+    v = wabi_cdr((wabi_pair) v);
+  }
+}
+
+#endif
